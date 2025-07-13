f0137e3dbe5988d8f4411bce7372fd60a38e492a
Loris Pandaru <loris.buchelet@gmail.com>
2025-07-13 15:28:48 +0200

    save 13-07

---
+++ src/components/studentReport/studentReportPdf.tsx
@@ -0,0 +1,196 @@
+import { Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
+import { SkillLevelEnum, StudentTestByStudentInterface } from '../../interfaces/student-test.interface';
+import StudentInterface from '../../interfaces/student.interface';
+import { getAverageSkillById } from '../../utils/calculations/average.function';
+
+interface StudentReportPdfProps {
+  tests: StudentTestByStudentInterface[];
+  student: StudentInterface;
+  uniqueSkills: { id: number; name: string }[];
+  average: number;
+}
+
+export default function StudentReportPdf(props: StudentReportPdfProps) {
+    const { tests, student, uniqueSkills, average } = props;
+
+    const styles = StyleSheet.create({
+        page: {
+            padding: 20,
+            fontSize: 12,
+        },
+        header: {
+            marginBottom: 10,
+        },
+        headerBoxes: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        },
+        headerBox : {
+            padding: 5,
+            borderStyle: 'solid',
+            borderColor: 'black',
+            borderWidth: 1
+        },
+        section: {
+            marginBottom: 20,
+        },
+        title: {
+            fontSize: 18,
+            marginBottom: 10,
+            fontWeight: 'bold',
+            textAlign: 'center'
+        },
+        table: {
+            display: 'flex',
+            flexDirection: 'column',
+            borderWidth: 1,
+            borderColor: '#000',
+            borderStyle: 'solid',
+        },
+        tableRow: {
+            flexDirection: 'row',
+            borderBottomWidth: 1,
+            borderBottomColor: '#000',
+            borderBottomStyle: 'solid',
+        },
+        tableHeader: {
+            backgroundColor: '#eee',
+            fontWeight: 'bold',
+        },
+        tableCell: {
+            padding: 4,
+            fontSize: 10,
+            borderRightWidth: 1,
+            borderRightColor: '#000',
+            borderRightStyle: 'solid',
+            textAlign: 'center',
+        },
+        lastCell: {
+            borderRightWidth: 0,
+        },
+        cellTR: { flex: 0.5 },
+        cellName: { flex: 2 },
+        cellNote: { flex: 0.5 },
+        cellSkill: { flex: 1 },
+        skillLevelWrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 4,
+        },
+        skillDot: {
+            width: 6,
+            height: 6,
+            borderRadius: 9999,
+            marginRight: 4,
+        },
+        comment: {
+            marginTop: 10,
+            padding: 5,
+            borderStyle: 'solid',
+            borderColor: 'black',
+            borderWidth: 2
+        }
+    });
+
+    function getSkillColor(level: SkillLevelEnum): string {
+        switch (level) {
+            case SkillLevelEnum.LVL0: return "#000000";
+            case SkillLevelEnum.LVL1: return "#F46030";
+            case SkillLevelEnum.LVL2: return "#FAC215";
+            case SkillLevelEnum.LVL3: return "#54C3B2";
+            case SkillLevelEnum.LVL4: return "#558F72"; 
+            case SkillLevelEnum.NN:
+            default:
+            return "#f0f9ff"; 
+        }
+    }
+
+    // Build skill average and level data
+    const skills = uniqueSkills.map(skill => ({
+        id: skill.id,
+        name: skill.name,
+        result: getAverageSkillById(tests, skill.id),
+    }));
+
+    return (
+    <Document>
+      <Page size="A4" orientation="landscape" style={styles.page}>
+        <View style={styles.header}>
+          <Text style={styles.title}>
+            Résultats de {student.firstName} {student.lastName.toUpperCase()} {student.classes ? `(${student.classes[0]})` : ''}
+          </Text>
+          <View style={styles.headerBoxes}>
+            <View style={styles.headerBox}>
+                <Text>Moyenne générale : {average} / 20</Text>
+                {skills.map((skill) => (
+                <Text key={skill.id}>
+                    {skill.name} : **/4
+                </Text>
+              ))}
+            </View>
+            <View style={styles.headerBox}>
+                <Text>Diagramme</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.section}>
+          <View style={styles.table}>
+
+            {/* Header */}
+            <View style={[styles.tableRow, styles.tableHeader]}>
+              <Text style={[styles.tableCell, styles.cellTR]}>TR</Text>
+              <Text style={[styles.tableCell, styles.cellName]}>Évaluation</Text>
+              <Text style={[styles.tableCell, styles.cellNote]}>Note</Text>
+              {skills.map((skill) => (
+                <Text
+                  key={skill.id}
+                  style={[
+                    styles.tableCell,
+                    styles.cellSkill,
+                  ]}
+                >
+                  {skill.name}
+                </Text>
+              ))}
+            </View>
+
+            {/* Rows */}
+            {tests.map(test => (
+              <View key={test.id} style={styles.tableRow}>
+                <Text style={[styles.tableCell, styles.cellTR]}>{test.test.trimester}</Text>
+                <Text style={[styles.tableCell, styles.cellName]}>{test.test.name}</Text>
+                <Text style={[styles.tableCell, styles.cellNote]}>{test.mark ?? 'x'}/{test.test.scale}</Text>
+                {skills.map((skill) => {
+                    const result = test.studenttesthasskill.find((sths => sths.skill.id === skill.id));
+                    const color = getSkillColor(skill.result.level);
+                  return (
+                    <View
+                        key={skill.id}
+                        style={[
+                            styles.tableCell,
+                            styles.cellSkill,
+                        ]}
+                    >
+                        <View style={styles.skillLevelWrapper}>
+                            {result && (<View style={[styles.skillDot, { backgroundColor: color }]} />)}
+                            <Text>{result?.level ?? "X"}</Text>
+                        </View>
+                    </View>
+                  );
+                })}
+              </View>
+            ))}
+
+          </View>
+          <View style={styles.comment}>
+            <Text style={{fontWeight: 700, paddingBottom: 300}}> Appréciation générale : </Text>
+            <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error, iusto expedita, enim quisquam obcaecati beatae blanditiis dicta adipisci suscipit in nesciunt temporibus deserunt quaerat! Unde, maxime architecto quas, alias adipisci earum deserunt error corrupti nostrum voluptates nihil, animi dolore voluptatibus similique quo possimus totam odit eos sunt vel reiciendis impedit. Corrupti quae iste ratione molestiae veritatis, assumenda maxime culpa aut!</Text>
+          </View>
+        </View>
+      </Page>
+    </Document>
+  );
+}
--- src/pages/students/studentDetails.page.tsx
+++ src/pages/students/studentDetails.page.tsx
@@ -11,6 +11,8 @@ import { SkillLevelEnum, StudentTestByStudentInterface } from "../../interfaces/
 import CheckBoxListItem from "../../components/ui/formInput/checkboxListItem";
 import { TrimesterEnum } from "../../interfaces/test.interface";
 import DiagramModal from "../../components/ui/DiagramModal";
+import { PDFDownloadLink } from '@react-pdf/renderer';
+import StudentReportPdf from "../../components/studentReport/studentReportPdf";
 
 export default function StudentDetailsPage() {
 
@@ -62,66 +64,75 @@ export default function StudentDetailsPage() {
                         </IconButton>
                     </div>
                     <div className="flex flex-col lg:flex-row lg:gap-3">
-                            <div className="mt-5 bg-white rounded-xl p-3 flex-row items-center justify-between w-full">
-                                <Typography as="h3" className="font-logo text-center">Moyenne annuelle</Typography>
-                                {studentTests.length > 0 ? (
-                                <div className="flex justify-between items-center">
-                                    <div className="flex flex-col justify-center">
-                                        <div><span className="font-semibold">Note</span> : {average} / 20</div>
-                                        <ul>
-                                            {uniqueSkills.map((sk) => (
-                                                <li key={sk.id} className="flex gap-2 items-center">
-                                                    <SkillBubble 
-                                                        letter={sk.abbr}
-                                                        level={getAverageSkillById(studentTests, sk.id).level}
-                                                    />
-                                                    <span className="font-semibold">{sk.name}</span> : {isNaN(getAverageSkillById(studentTests, sk.id).average) ? "Non évalué" : getAverageSkillById(studentTests, sk.id).average + "/4"}
-                                                </li>
-                                            ))}
-                                        </ul>
+                        <div className="mt-5 bg-white rounded-xl p-3 flex-row items-center justify-between w-full">
+                            <Typography as="h3" className="font-logo text-center">Moyenne annuelle</Typography>
+                            {studentTests.length > 0 ? (
+                            <div className="flex justify-between items-center">
+                                <div className="flex flex-col justify-center">
+                                    <div>
+                                        <span className="font-semibold">Note</span> : {average} / 20
                                     </div>
-                                    <div className="grid grid-cols-2 gap-2">
-                                        <IconButton onClick={handleDiagramModal}>
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" fillRule="evenodd" d="M14 20.5V4.25c0-.728-.002-1.2-.048-1.546c-.044-.325-.115-.427-.172-.484s-.159-.128-.484-.172C12.949 2.002 12.478 2 11.75 2s-1.2.002-1.546.048c-.325.044-.427.115-.484.172s-.128.159-.172.484c-.046.347-.048.818-.048 1.546V20.5z" clipRule="evenodd"/><path fill="currentColor" d="M8 8.75A.75.75 0 0 0 7.25 8h-3a.75.75 0 0 0-.75.75V20.5H8zm12 5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75v6.75H20z" opacity="0.7"/><path fill="currentColor" d="M1.75 20.5a.75.75 0 0 0 0 1.5h20a.75.75 0 0 0 0-1.5z" opacity="0.5"/></svg>
-                                        </IconButton>
-                                        <IconButton disabled>
+                                    <ul>
+                                        {uniqueSkills.map((sk) => (
+                                            <li key={sk.id} className="flex gap-2 items-center">
+                                                <SkillBubble 
+                                                    letter={sk.abbr}
+                                                    level={getAverageSkillById(studentTests, sk.id).level}
+                                                />
+                                                <span className="font-semibold">{sk.name}</span> : {isNaN(getAverageSkillById(studentTests, sk.id).average) ? "Non évalué" : getAverageSkillById(studentTests, sk.id).average + "/4"}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                                <div className="grid grid-cols-2 gap-2">
+                                    <IconButton onClick={handleDiagramModal}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" fillRule="evenodd" d="M14 20.5V4.25c0-.728-.002-1.2-.048-1.546c-.044-.325-.115-.427-.172-.484s-.159-.128-.484-.172C12.949 2.002 12.478 2 11.75 2s-1.2.002-1.546.048c-.325.044-.427.115-.484.172s-.128.159-.172.484c-.046.347-.048.818-.048 1.546V20.5z" clipRule="evenodd"/><path fill="currentColor" d="M8 8.75A.75.75 0 0 0 7.25 8h-3a.75.75 0 0 0-.75.75V20.5H8zm12 5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75v6.75H20z" opacity="0.7"/><path fill="currentColor" d="M1.75 20.5a.75.75 0 0 0 0 1.5h20a.75.75 0 0 0 0-1.5z" opacity="0.5"/></svg>
+                                    </IconButton>
+                                    <PDFDownloadLink 
+                                        document={<StudentReportPdf
+                                            tests={filteredTests} student={student} uniqueSkills={uniqueSkills} average={average}
+                                        />} 
+                                    fileName={`${student.lastName}_${student.firstName}.pdf`}
+                                    >
+                                        <IconButton>
                                             <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><g fill="none" fillRule="evenodd"><path d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"></path><path fill="currentColor" d="M13.586 2a2 2 0 0 1 1.284.467l.13.119L19.414 7a2 2 0 0 1 .578 1.238l.008.176V20a2 2 0 0 1-1.85 1.995L18 22H6a2 2 0 0 1-1.995-1.85L4 20V4a2 2 0 0 1 1.85-1.995L6 2zM12 4H6v16h12V10h-4.5a1.5 1.5 0 0 1-1.493-1.356L12 8.5zm.988 7.848a6.22 6.22 0 0 0 2.235 3.872c.887.717.076 2.121-.988 1.712a6.22 6.22 0 0 0-4.47 0c-1.065.41-1.876-.995-.989-1.712a6.22 6.22 0 0 0 2.235-3.872c.178-1.127 1.8-1.126 1.977 0m-.99 2.304l-.688 1.196h1.38zM14 4.414V8h3.586z"></path></g></svg>
                                         </IconButton>
-                                        <IconButton disabled>
-                                            <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} viewBox="0 0 32 32"><path fill="currentColor" d="M10 18h8v2h-8zm0-5h12v2H10zm0 10h5v2h-5z"></path><path fill="currentColor" d="M25 5h-3V4a2 2 0 0 0-2-2h-8a2 2 0 0 0-2 2v1H7a2 2 0 0 0-2 2v21a2 2 0 0 0 2 2h18a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2M12 4h8v4h-8Zm13 24H7V7h3v3h12V7h3Z"></path></svg>
-                                        </IconButton>
-                                        <IconButton disabled>
-                                            <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><path fill="currentColor" d="M4 20q-.825 0-1.412-.587T2 18V6q0-.825.588-1.412T4 4h16q.825 0 1.413.588T22 6v12q0 .825-.587 1.413T20 20zm8-7l8-5V6l-8 5l-8-5v2z"></path></svg>
-                                        </IconButton>
-                                    </div>
+                                    </PDFDownloadLink>
+                                    <IconButton disabled>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} viewBox="0 0 32 32"><path fill="currentColor" d="M10 18h8v2h-8zm0-5h12v2H10zm0 10h5v2h-5z"></path><path fill="currentColor" d="M25 5h-3V4a2 2 0 0 0-2-2h-8a2 2 0 0 0-2 2v1H7a2 2 0 0 0-2 2v21a2 2 0 0 0 2 2h18a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2M12 4h8v4h-8Zm13 24H7V7h3v3h12V7h3Z"></path></svg>
+                                    </IconButton>
+                                    <IconButton disabled>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><path fill="currentColor" d="M4 20q-.825 0-1.412-.587T2 18V6q0-.825.588-1.412T4 4h16q.825 0 1.413.588T22 6v12q0 .825-.587 1.413T20 20zm8-7l8-5V6l-8 5l-8-5v2z"></path></svg>
+                                    </IconButton>
                                 </div>
-                                ):(
-                                <div className="flex flex-col p-3">
-                                    <span className="text-center">Cet élève n'a pas encore été évalué.</span>
-                                    <Button 
-                                        onClick={() => navigate(`/tests/new`)}
-                                        className="bg-test-300 text-black"
-                                    >Ajoutez une évaluation</Button>
-                                </div>
-                                )}
                             </div>
-                            <div className="mt-5 bg-white rounded-xl p-3 w-full">
-                                <Typography as="h3" className="font-logo text-center">Filtrer</Typography>
-                                <fieldset className="border-2 border-dashed m-3 rounded-xl">
-                                    <legend className="ms-3">Filtrer par trimestre</legend>
-                                    <List className="flex-row ">
-                                    { Object.values(TrimesterEnum).map(trimester =>
-                                        (<CheckBoxListItem 
-                                            key={trimester}
-                                            onClick={() => handleTrimesterFilters(trimester)}
-                                            checked={trimesterFilters.includes(trimester)}
-                                            id={trimester}
-                                            label={trimester}
-                                        />))}
-                                    </List>
-                                </fieldset>
+                            ):(
+                            <div className="flex flex-col p-3">
+                                <span className="text-center">Cet élève n'a pas encore été évalué.</span>
+                                <Button 
+                                    onClick={() => navigate(`/tests/new`)}
+                                    className="bg-test-300 text-black"
+                                >Ajoutez une évaluation</Button>
                             </div>
+                            )}
                         </div>
+                        <div className="mt-5 bg-white rounded-xl p-3 w-full">
+                            <Typography as="h3" className="font-logo text-center">Filtrer</Typography>
+                            <fieldset className="border-2 border-dashed m-3 rounded-xl">
+                                <legend className="ms-3">Filtrer par trimestre</legend>
+                                <List className="flex-row ">
+                                { Object.values(TrimesterEnum).map(trimester =>
+                                    (<CheckBoxListItem 
+                                        key={trimester}
+                                        onClick={() => handleTrimesterFilters(trimester)}
+                                        checked={trimesterFilters.includes(trimester)}
+                                        id={trimester}
+                                        label={trimester}
+                                    />))}
+                                </List>
+                            </fieldset>
+                        </div>
+                    </div>
                 </Card>
                 <Card className="mt-3 p-3 h-full w-full overflow-scroll text-black">
                     <table className="w-full min-w-max table-auto text-left">
