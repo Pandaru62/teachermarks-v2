0a8c7dcd9ade61e549ddf5e9fba9564beca2576d
Loris Pandaru <loris.buchelet@gmail.com>
2025-09-25 18:07:35 +0200

    solved logo issue

--- src/components/ui/header/navbar.tsx
+++ src/components/ui/header/navbar.tsx
@@ -27,6 +27,7 @@ import { useMutation } from "@tanstack/react-query";
 import { logOutClearCookies } from "../../../pages/auth/service/auth.service";
 import { AxiosError } from "axios";
 import NavBarLink from "./NavBarLink";
+import logo from '../assets/Logo_TeacherMarks/OrangeM.png';
 
 // profile menu component
 const profileMenuItems = [
@@ -253,7 +254,7 @@ export default function Header() {
           href="/"
           className="mr-4 ml-2 cursor-pointer py-1.5 font-medium"
         >
-            <img src="src\assets\Logo_TeacherMarks\OrangeM.png" alt="logo" className="w-[205px] h-auto"/>
+            <img src={logo} alt="logo" className="w-[205px] h-auto"/>
         </Typography>
         {isAuthenticated ? (
         <>
