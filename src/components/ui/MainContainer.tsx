334a0678ff6925a68a0387ba19fe24f252d3689c
Loris Pandaru <loris.buchelet@gmail.com>
2026-09-15 19:01:24 +0200

    fix(dashboard): commented test on classes

--- src/components/ui/MainContainer.tsx
+++ src/components/ui/MainContainer.tsx
@@ -6,7 +6,7 @@ interface MainContainerProps {
 
 export default function MainContainer({ children }: MainContainerProps) {
   return (
-    <div className="mx-2 mb-16">
+    <div className="mx-2 mb-24">
       {children}
     </div>
   );
--- src/pages/Home/Home.loggeduser.tsx
+++ src/pages/Home/Home.loggeduser.tsx
@@ -50,7 +50,7 @@ export default function HomeLoggedUser() {
             </div>
 
             {/* Mini cards classes */}
-            <Card className="rounded-2xl shadow-md p-5">
+            {/* <Card className="rounded-2xl shadow-md p-5">
                 <div className="flex items-center gap-2 mb-4">
                     <UsersIcon className="w-5 h-5 text-test-400" />
                     <Typography as="h2" className="text-xl font-semibold">
@@ -58,7 +58,7 @@ export default function HomeLoggedUser() {
                     </Typography>
                 </div>
                 <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
-                    {/* {dashboard?.schoolClasses.map((sc) => ( */}
+                    {dashboard?.schoolClasses.map((sc) => ( 
                     {[
                         { id: 1, name: "6E2", studentCount: 27, testCount: 2, color: "#54C3B2" },
                         { id: 2, name: "6E4", studentCount: 25, testCount: 3, color: "#F46030" },
@@ -94,7 +94,7 @@ export default function HomeLoggedUser() {
                         </Link>
                     ))}
                 </div>
-            </Card>
+            </Card> */}
 
             {/* Dernières évaluations */}
             <Card className="rounded-2xl shadow-md p-5">
